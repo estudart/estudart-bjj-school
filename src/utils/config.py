3efa14548d97e578bbd90daaaac1b10eeff034fd
estudart <erico.studartdf@gmail.com>
2025-03-28 16:30:41 -0300

    feat: telegram adapter implementation

---
+++ src/utils/config.py
@@ -0,0 +1,3 @@
+# -------------- SLACK SECRETS ----------------------------------
+SLACK_BOT_TOKEN = "7202492523:AAEOTnYNgy6rvB04RwUFbZinh-g-6K7CXLw"
+SLACK_API_URL = "https://api.telegram.org/bot"
--- src/utils/extensions.py
+++ src/utils/extensions.py
@@ -1,3 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
 
+from adapters.telegram_adapter import TelegramAdapter
+
 db = SQLAlchemy()
+telegram_adapter = TelegramAdapter()
