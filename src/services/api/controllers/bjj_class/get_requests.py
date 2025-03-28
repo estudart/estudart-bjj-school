9697d29504fd31d3479f747b1b54c774ef00178c
estudart <erico.studartdf@gmail.com>
2025-03-28 11:58:29 -0300

    feat: crud of bjj classes

--- src/services/api/controllers/bjj_class/get_requests.py
+++ src/services/api/controllers/bjj_class/get_requests.py
@@ -3,9 +3,97 @@ import logging
 from flask import make_response
 
 from services.api.models.bjj_class import BJJClass
+from services.api.models.student_class import StudentClasses
+from services.api.models.student import Student
 from utils.extensions import db
 
 
 
 def get_class_by_id(id):
-    pass
+    try:
+        search_class = (
+            db.session.query(BJJClass)
+            .filter(BJJClass.id==id)
+            .first()
+        )
+        if not search_class:
+            return make_response(
+                {
+                    "message": "class was not found in database"
+                }, 404)
+        
+        dict_class = search_class.to_dict()
+        logging.info(
+            f"class was found, {dict_class}")
+        return make_response(
+            {
+                "message": "class found",
+                "data": search_class.to_dict()
+            }, 200)
+    
+    except Exception as err:
+        logging.error(f"Could not find class, reason: {err}")
+        return make_response(
+            {
+                "message": f"Could not find class, reason: {err}"
+            }, 500)        
+
+
+def get_all_classes():
+    try:
+        search_classes = (
+            db.session.query(BJJClass)
+            .all()
+        )
+        if not search_classes:
+            return make_response(
+                {
+                    "message": "classes were not found in database"
+                }, 404)
+        
+        dict_class = [search_class.to_dict() for search_class in search_classes]
+        logging.info(
+            f"class was found, {dict_class}")
+        return make_response(
+            {
+                "message": "class found",
+                "data": dict_class
+            }, 200)
+    
+    except Exception as err:
+        logging.error(f"Could not find classes, reason: {err}")
+        return make_response(
+            {
+                "message": f"Could not find classes, reason: {err}"
+            }, 500) 
+
+
+def get_students_by_class_id(class_id):
+    try:
+        search_students = (
+            db.session.query(Student)
+            .join(StudentClasses)
+            .filter(StudentClasses.class_id==class_id)
+            .all()
+        )
+        if not search_students:
+            return make_response(
+                {
+                    "message": "class was not found in database"
+                }, 404)
+        
+        dict_students = [search_student.to_dict() for search_student in search_students]
+        logging.info(
+            f"students were found, {dict_students}")
+        return make_response(
+            {
+                "message": "students were found",
+                "data": dict_students
+            }, 200)
+    
+    except Exception as err:
+        logging.error(f"Could not find class, reason: {err}")
+        return make_response(
+            {
+                "message": f"Could not find class, reason: {err}"
+            }, 500)
--- src/services/api/controllers/bjj_class/post_requests.py
+++ src/services/api/controllers/bjj_class/post_requests.py
@@ -4,6 +4,7 @@ from datetime import datetime
 from flask import make_response
 
 from services.api.models.bjj_class import BJJClass
+from services.api.models.student_class import StudentClasses
 from utils.extensions import db
 
 
@@ -27,4 +28,22 @@ def post_class(data):
         logging.error(err)
         return make_response(
             {"message": f"Failed to add new class: {err}"}, 
+            500)
+    
+
+def add_student_to_class(data):
+    try:
+        new_student_class = StudentClasses(**data)
+        db.session.add(new_student_class)
+        db.session.commit()
+        return make_response(
+            {"message": "Successfully added student to class"}, 
+            200)
+    except ValueError as err:
+        return make_response(
+            {"message": f"Inavlid json: {err}"}, 
+            400)
+    except Exception as err:
+        return make_response(
+            {"message": f"Failed to add student to class: {err}"}, 
             500)
