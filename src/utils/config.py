ada9982898170a30d4c4e64642c5dfc0d850f8d7
estudart <erico.studartdf@gmail.com>
2025-03-28 16:36:34 -0300

    fix: remove config.py

--- src/utils/config.py
+++
@@ -1,3 +0,0 @@
-# -------------- TELEGRAM SECRETS ----------------------------------
-TELEGRAM_BOT_TOKEN = "7202492523:AAEOTnYNgy6rvB04RwUFbZinh-g-6K7CXLw"
-TELEGRAM_API_URL = "https://api.telegram.org/bot"
