f62fba2115fad7b93ddbd7a73698e8b944071b56
estudart <erico.studartdf@gmail.com>
2025-04-01 09:45:21 -0300

    fix: remove comments from celery

--- src/services/celery/celery_app.py
+++ src/services/celery/celery_app.py
@@ -10,6 +10,7 @@ def make_celery(app):
         backend=app.config['result_backend'],
         include=["services.celery.tasks"])
     celery.conf.update(app.config)
+
     TaskBase = celery.Task
     class ContextTask(TaskBase):
         abstract = True
@@ -24,25 +25,5 @@ def make_celery(app):
             "schedule": 10.0,  # Runs every 10 seconds
         },
     }
+
     return celery
-
-
-# from celery import Celery
-# from celery.schedules import crontab
-
-# def make_celery(app):
-#     celery = Celery(
-#         app.import_name,
-#         broker=app.config['CELERY_BROKER_URL'],
-#         backend=app.config['CELERY_RESULT_BACKEND']
-#     )
-#     celery.conf.update(app.config)
-
-#     celery.conf.beat_schedule = {
-#         "check_and_send_reminders": {
-#             "task": "services.celery.tasks.check_and_send_reminders",
-#             "schedule": crontab(minute=0, hour=9),  # Run every day at 9 AM
-#         },
-#     }
-
-#     return celery
